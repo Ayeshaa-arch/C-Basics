0692eb4985397d334f962af99dc0afe5838ac970
ayeshaa-arch <ayeshaahmad1955@gamil.com>
2025-09-03 12:51:04 +0500

    dec

---
+++ .Dec/dec 1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a = 22;
+    int b = a;
+    int c = b * 6 ;
+    int d = 1 , e;
+
+    int oldAge = 22;
+    int year = 2;
+    int newAge = oldAge + year ;
+
+    int  x , y , z;
+    x = y = z = 4;
+    
+        return 0;
+}
