4274a39c62125dc061ebbd1dd9aff0f3afa0d3a3
ayeshaa-arch <ayeshaahmad1955@gamil.com>
2025-09-29 18:26:02 +0500

    practice

---
+++ Q7b.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<math.h>
+
+int main () {
+   
+   int x ; int y = x;
+   return 0; 
+}
---
+++ Q8b.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+
+int main () {
+  int ismonday = 1;
+  int israining = 1;
+  printf("%d \n", ismonday || israining );
+   return 0; 
+}
