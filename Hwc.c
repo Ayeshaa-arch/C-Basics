791a453ed6826919ad723b54cfcd9dbcc4c3a766
ayeshaa-arch <ayeshaahmad1955@gamil.com>
2025-09-30 19:21:32 +0500

    homework

---
+++ Hwc.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+// program to print the smallest number
+
+int main () {
+     int num1 , num2 , num3 , smallest ;
+
+        printf("Enter three numbers : ");
+        scanf("%d %d %d", &num1 , &num2 , &num3);
+
+        smallest = num1;
+
+        if (num2 < smallest)
+            smallest = num2;
+        if (num3 < smallest)
+            smallest = num3;
+
+            printf("The smallest number is : %d\n" , smallest);
+   return 0; 
+}
